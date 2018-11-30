2c967d125259d92933be4a3f370984d4fead7f70
Ryan Zegray <ryan.zegray@gmail.com>
2018-11-30 17:04:38 -0500

    use http-errors for error handliong

--- app.js
+++ app.js
@@ -22,22 +22,18 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+  next(createError.NotFound());
 });
 
 // error handler
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send({
-    message: err.message || 'Internal server error',
-    // if in development include the stack trace
-    stack: req.app.get('env') === 'development' ? err.stack : undefined,
-  });
+  // if err message is safe to expose to client
+  if (err.expose === true) {
+    res.status(err.status || 500).send(err);
+  } else {
+    res.status(500).send(createError.InternalServerError());
+  }
 });
 
 module.exports = app;
