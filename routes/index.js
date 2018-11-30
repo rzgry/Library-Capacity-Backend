6e572665135251bbb4964910929e7dfce12d2db3
Ryan Zegray <ryan.zegray@gmail.com>
2018-11-30 16:51:10 -0500

    add eslint

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+// eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
-  res.send({ okay: true });
+  res.send({ message: 'Hello world' });
 });
 
 module.exports = router;
