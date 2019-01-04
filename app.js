360e4a8fba8ac28753fd6e7995409b9f4961b92e
Ryan Zegray <ryan.zegray@gmail.com>
2019-01-04 13:42:42 -0500

    seperate error handler middleware

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
@@ -8,16 +7,15 @@ const helmet = require('helmet');
 const indexRouter = require('./routes/index');
 const librariesRouter = require('./routes/libraries');
 
+const errorHandler = require('./middleware/errorHandler');
+
 const app = express();
 
-// https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
-app.use(helmet());
-
+app.use(helmet()); // https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/libraries', librariesRouter);
@@ -27,16 +25,7 @@ app.use((req, res, next) => {
   next(createError.NotFound());
 });
 
-// error handler
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  // if err message is safe to expose to client
-  // or we are running in development mode
-  if (err.expose === true || process.env.NODE_ENV === 'development') {
-    res.status(err.status || 500).send(err);
-  } else {
-    res.status(500).send(createError.InternalServerError());
-  }
-});
+// pass any errors to the error handler
+app.use(errorHandler);
 
 module.exports = app;
